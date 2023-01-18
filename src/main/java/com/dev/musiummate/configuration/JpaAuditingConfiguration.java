8cac1c50b2b7976fae3e87c9af3e630c487d37bb
Jiyoung <ahn.jiyoung15@gmail.com>
2023-01-18 13:23:42 +0900

    Feat: JpaAuditing 기능 추가

---
+++ src/main/java/com/dev/musiummate/configuration/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.dev.musiummate.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
