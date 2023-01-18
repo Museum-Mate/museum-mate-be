a749e58ec8fcb132e81c8d1b3939e4d0b0b26d29
leejoonsang <djm04212@gmail.com>
2023-01-18 17:55:00 +0900

    feat: NOT_FOUND_POST 상수 추가

--- src/main/java/com/dev/musiummate/exception/ErrorCode.java
+++ src/main/java/com/dev/musiummate/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    DUPLICATE_USERNAME(HttpStatus.CONFLICT, "User name conflict");
+    DUPLICATE_USERNAME(HttpStatus.CONFLICT, "User name conflict"),
+    NOT_FOUND_POST(HttpStatus.NOT_FOUND, "Post Not Found");
 
     private HttpStatus httpStatus;
     private String message;
