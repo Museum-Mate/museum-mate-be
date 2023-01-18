f68d0112420f2b41aae4d85cc4ea00b00e5f4e2d
KimJaeGeun <jgk9612@gmail.com>
2023-01-19 02:04:28 +0900

    refactor: Clean Code 을 위한 변경

--- src/main/java/com/dev/museummate/exception/ErrorCode.java
+++ src/main/java/com/dev/museummate/exception/ErrorCode.java
@@ -10,10 +10,7 @@ public enum ErrorCode {
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "User name conflict"),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "email conflict"),
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND,"email not found"),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "invalid password"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid"),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다" ),
-    ;
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "invalid password");
 
     private HttpStatus httpStatus;
     private String message;
