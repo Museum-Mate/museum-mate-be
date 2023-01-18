7b42eb36d75fcaced4a9ebc1bf878de22d84e747
Jiyoung <ahn.jiyoung15@gmail.com>
2023-01-18 13:22:05 +0900

    Rename: UserEntity 경로 변경

--- src/main/java/com/dev/musiummate/repository/UserRepository.java
+++ src/main/java/com/dev/musiummate/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.dev.musiummate.repository;
 
-import com.dev.musiummate.domain.UserEntity;
+import com.dev.musiummate.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
