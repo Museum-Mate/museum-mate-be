b2423cb05383bfc140001b9ef196182c0e4b9717
Jiyoung <ahn.jiyoung15@gmail.com>
2023-01-18 10:44:53 +0900

    Feat: JwtUtils 클래스 정의

--- src/main/java/com/dev/musiummate/utils/JwtUtils.java
+++ src/main/java/com/dev/musiummate/utils/JwtUtils.java
@@ -1,6 +1,8 @@
 package com.dev.musiummate.utils;
 
 import com.dev.musiummate.service.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,12 @@ public class JwtUtils {
 
     private final UserService userService;
 
+    private static Claims extractClaims(String token, String secretKey){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+
     public static String createToken(String userName, String secretKey, long expiredTimeMs){
         return "";
     }
+
 }
