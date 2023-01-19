4d7b6bb2262721e53e2fdb49b4296701d94f8ce3
chlalswns200 <chlalswns200@naver.com>
2023-01-19 10:42:55 +0900

    refacto: 만료시간 정보 jwtUtils에서 가지고 있는 방식으로 변경했습니다.

--- src/main/java/com/dev/museummate/utils/JwtUtils.java
+++ src/main/java/com/dev/museummate/utils/JwtUtils.java
@@ -15,20 +15,35 @@ import java.util.Date;
 public class JwtUtils {
     private final UserService userService;
 
+    private static final long accessExpireTimeMs = 1000 * 60 * 5;
+
+    private static final long refreshExpireTimeMs = 1000 * 60 * 30;
+
     private static Claims extractClaims(String token, String secretKey){
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
     }
 
-    public static String createAccessToken(String email, String secretKey, long expiredTimeMs){
+    public static String createAccessToken(String email, String secretKey){
         Claims claims = Jwts.claims();  //토큰의 내용에 값을 넣기 위해 Claims 객체 생성
         claims.put("email", email);
 
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiredTimeMs))
+                .setExpiration(new Date(System.currentTimeMillis() + accessExpireTimeMs))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
+    public static String createRefreshToken(String email, String secretKey) {
+        Claims claims = Jwts.claims();  //토큰의 내용에 값을 넣기 위해 Claims 객체 생성
+        claims.put("email", email);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpireTimeMs))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
 }
