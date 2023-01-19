f4f139a6f367c3416f3cae04f2aa68271fe8e69d
leejoonsang <djm04212@gmail.com>
2023-01-19 17:06:16 +0900

    feat: USERNAME_NOT_FOUND 상수 추가

--- src/main/java/com/dev/museummate/exception/ErrorCode.java
+++ src/main/java/com/dev/museummate/exception/ErrorCode.java
@@ -9,9 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "User name conflict"),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "email conflict"),
+    NOT_FOUND_POST(HttpStatus.NOT_FOUND, "Post Not Found"),
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND,"email not found"),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "invalid password");
-
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "invalid password"),
+    ;
     private HttpStatus httpStatus;
     private String message;
 }
