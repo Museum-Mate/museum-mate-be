9fca043786fbe0a5ccca92698af5e8b126b97edc
Jiyoung <ahn.jiyoung15@gmail.com>
2023-01-19 17:18:26 +0900

    Feat: Spring Security Filter 수정
    - 정의되지 않은 url 접근시 token 유무에 상관없이 MethodNotAllowed 반환하도록 수정
    - 허용되지 않은 Http Method로 접근시 token 유무에 상관없이 NotFound 반환하도록 수정

--- src/main/java/com/dev/museummate/security/JwtExceptionFilter.java
+++ src/main/java/com/dev/museummate/security/JwtExceptionFilter.java
@@ -25,34 +25,28 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
             filterChain.doFilter(request,response);
         } catch (MalformedJwtException e){
             log.info("손상된 토큰입니다: {}", request.getHeader(HttpHeaders.AUTHORIZATION));
-            setResponse(response,ErrorCode.INVALID_TOKEN);
+            request.setAttribute("errorCode", ErrorCode.INVALID_TOKEN);
+            filterChain.doFilter(request, response);
         } catch (UnsupportedJwtException e){
             log.info("지원하지 않는 토큰입니다: {}", request.getHeader(HttpHeaders.AUTHORIZATION));
-            setResponse(response,ErrorCode.INVALID_TOKEN);
+            request.setAttribute("errorCode", ErrorCode.INVALID_TOKEN);
+            filterChain.doFilter(request, response);
         } catch (SignatureException e){
             log.info("시그니처 검증에 실패한 토큰입니다: {}",request.getHeader(HttpHeaders.AUTHORIZATION));
-            setResponse(response,ErrorCode.INVALID_TOKEN);
+            request.setAttribute("errorCode", ErrorCode.INVALID_TOKEN);
+            filterChain.doFilter(request, response);
         } catch(ExpiredJwtException e) {
             log.info("만료된 토큰입니다: {}",request.getHeader(HttpHeaders.AUTHORIZATION));
-            setResponse(response,ErrorCode.EXPIRED_TOKEN);
+            request.setAttribute("errorCode", ErrorCode.EXPIRED_TOKEN);
+            filterChain.doFilter(request, response);
         } catch (IllegalArgumentException | JwtException e){
             log.info("잘못된 토큰입니다: {}",request.getHeader(HttpHeaders.AUTHORIZATION));
-            setResponse(response,ErrorCode.INVALID_TOKEN);
-        } catch (AppException e){   //유저가 존재하지 않는 경우
+            request.setAttribute("errorCode", ErrorCode.INVALID_TOKEN);
+            filterChain.doFilter(request, response);
+        } catch (AppException e){   // 유저가 존재하지 않는 경우
             log.info("App Exception: {}",request.getHeader(HttpHeaders.AUTHORIZATION));
-            setResponse(response,e.getErrorCode());
+            request.setAttribute("errorCode", e.getErrorCode());
+            filterChain.doFilter(request, response);
         }
-    }
-
-    private void setResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        response.setStatus(errorCode.getHttpStatus().value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setCharacterEncoding("utf-8");
-
-        Response errorResponse = Response.error(new ErrorResponse(errorCode.toString(), errorCode.getMessage()));
-
-        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
