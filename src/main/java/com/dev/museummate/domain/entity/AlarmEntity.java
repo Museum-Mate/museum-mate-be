1be49d17632a9ed9d365b8cea344b386fbe72b97
chlalswns200 <chlalswns200@naver.com>
2023-01-26 15:53:54 +0900

    feat: entity 추가 Alarm,comment,review 추가

---
+++ src/main/java/com/dev/museummate/domain/entity/AlarmEntity.java
@@ -0,0 +1,32 @@
+package com.dev.museummate.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "alarm")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AlarmEntity extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotNull
+    private String alarmMessage;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @NotNull
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "exhibition_id")
+    @NotNull
+    private ExhibitionEntity exhibition;
+
+
+}
---
+++ src/main/java/com/dev/museummate/domain/entity/CommentEntity.java
@@ -0,0 +1,30 @@
+package com.dev.museummate.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentEntity extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private UserEntity user;
+
+    @ColumnDefault("0")
+    @NotNull
+    private Long parentId;
+
+    @NotNull
+    private String content;
+
+}
