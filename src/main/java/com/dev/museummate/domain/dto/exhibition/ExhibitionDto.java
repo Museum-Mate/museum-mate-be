c6f949953bb25869c79b6e257a6bdee76ba0e1df
leejoonsang <djm04212@gmail.com>
2023-01-27 14:49:08 +0900

    feat: 필드 및 메소드 리팩토링

--- src/main/java/com/dev/museummate/domain/dto/exhibition/ExhibitionDto.java
+++ src/main/java/com/dev/museummate/domain/dto/exhibition/ExhibitionDto.java
@@ -2,6 +2,7 @@ package com.dev.museummate.domain.dto.exhibition;
 
 import com.dev.museummate.domain.entity.ExhibitionEntity;
 import com.dev.museummate.domain.entity.GalleryEntity;
+import com.dev.museummate.domain.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -20,14 +21,30 @@ public class ExhibitionDto {
     private String price;
     private String ageLimit;
     private String detailInfo;
-    private String galleryDetail;
+    private String galleryLocation;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "gallery_id")
     private GalleryEntity gallery;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    private String statMale;
+    private String statFemale;
+    private String statAge10;
+    private String statAge20;
+    private String statAge30;
+    private String statAge40;
+    private String statAge50;
+    private String mainImgUrl;
+    private String noticeImgUrl;
+    private String detailImgUrl;
+
     @Builder
-    public ExhibitionDto(Long id, String name, String startsAt, String endsAt, String price, String ageLimit, String detailInfo, String galleryDetail, GalleryEntity gallery) {
+    public ExhibitionDto(Long id, String name, String startsAt, String endsAt, String price, String ageLimit, String detailInfo, String galleryLocation, GalleryEntity gallery, UserEntity user,
+                         String statMale, String statFemale, String statAge10, String statAge20, String statAge30, String statAge40, String statAge50, String mainImgUrl, String noticeImgUrl, String detailImgUrl) {
         this.id = id;
         this.name = name;
         this.startsAt = startsAt;
@@ -35,8 +52,19 @@ public class ExhibitionDto {
         this.price = price;
         this.ageLimit = ageLimit;
         this.detailInfo = detailInfo;
-        this.galleryDetail = galleryDetail;
+        this.galleryLocation = galleryLocation;
         this.gallery = gallery;
+        this.user = user;
+        this.statMale = statMale;
+        this.statFemale = statFemale;
+        this.statAge10 = statAge10;
+        this.statAge20 = statAge20;
+        this.statAge30 = statAge30;
+        this.statAge40 = statAge40;
+        this.statAge50 = statAge50;
+        this.mainImgUrl = mainImgUrl;
+        this.noticeImgUrl = noticeImgUrl;
+        this.detailImgUrl = detailImgUrl;
     }
 
     /**
@@ -52,8 +80,19 @@ public class ExhibitionDto {
                 .price(exhibitionEntity.getPrice())
                 .ageLimit(exhibitionEntity.getAgeLimit())
                 .detailInfo(exhibitionEntity.getDetailInfo())
-                .galleryDetail(exhibitionEntity.getGalleryDetail())
+                .galleryLocation(exhibitionEntity.getGalleryLocation())
                 .gallery(exhibitionEntity.getGallery())
+                .user(exhibitionEntity.getUser())
+                .statMale(exhibitionEntity.getStatMale())
+                .statFemale(exhibitionEntity.getStatFemale())
+                .statAge10(exhibitionEntity.getStatAge10())
+                .statAge20(exhibitionEntity.getStatAge20())
+                .statAge30(exhibitionEntity.getStatAge30())
+                .statAge40(exhibitionEntity.getStatAge40())
+                .statAge50(exhibitionEntity.getStatAge50())
+                .mainImgUrl(exhibitionEntity.getMainImgUrl())
+                .noticeImgUrl(exhibitionEntity.getNoticeImgUrl())
+                .detailImgUrl(exhibitionEntity.getDetailImgUrl())
                 .build();
     }
 }
