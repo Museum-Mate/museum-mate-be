42fb5135be393fb1c117cf7d1cc0568380d7eabe
hyeonju01 <tangled6648@gmail.com>
2023-01-30 16:04:35 +0900

    Feat: ReviewService 파일 작성

--- src/main/java/com/dev/museummate/service/ReviewService.java
+++ src/main/java/com/dev/museummate/service/ReviewService.java
@@ -1,12 +1,15 @@
 package com.dev.museummate.service;
 
+import com.dev.museummate.domain.dto.review.ReviewDto;
 import com.dev.museummate.domain.dto.review.WriteReviewRequest;
 import com.dev.museummate.domain.dto.review.WriteReviewResponse;
 import com.dev.museummate.domain.entity.ExhibitionEntity;
+import com.dev.museummate.domain.entity.ReviewEntity;
 import com.dev.museummate.domain.entity.UserEntity;
 import com.dev.museummate.exception.AppException;
 import com.dev.museummate.exception.ErrorCode;
 import com.dev.museummate.repository.ExhibitionRepository;
+import com.dev.museummate.repository.ReviewRepository;
 import com.dev.museummate.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,17 +20,17 @@ public class ReviewService {
 
     private final UserRepository userRepository;
     private final ExhibitionRepository exhibitionRepository;
-    //private final ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
     /*
     Returns Response
      */
-    public WriteReviewResponse writeReview(String name,
+    public WriteReviewResponse writeReview(String email,
                                            WriteReviewRequest writeReviewRequest,
                                            Long exhibitionId) {
         // 유저 검증 (한 번 더 하는 것)
-        UserEntity user = userRepository.findByUserName(name)
-                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, "Username Not Found"));
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, String.format("Username %s Not Found.😢", email)));
 
         // 전시회 검증
         ExhibitionEntity exhibition = exhibitionRepository.findById(exhibitionId)
@@ -35,12 +38,24 @@ public class ReviewService {
 
 
         // 리뷰 등록
-        /*
-        로직
-        1. ReviewEntity 생성
-        2. ReviewEntity toDto 메서드로 Dto 객체 생성하여 Response 객체로 전달
-         */
 
-        return new WriteReviewResponse();
+        //1. ReviewEntity 생성
+        ReviewEntity review = ReviewEntity.builder()
+                .title(writeReviewRequest.getTitle())
+                .content(writeReviewRequest.getContent())
+                .star(writeReviewRequest.getStar())
+                .visitedDate(writeReviewRequest.getVisitedDate())
+                .user(user)
+                .exhibition(exhibition)
+
+                .build();
+
+        // db에 review를 저장
+        reviewRepository.save(review);
+
+        // ReviewDto 생성
+        ReviewDto reviewDto = ReviewDto.toDto(review);
+
+        return new WriteReviewResponse(reviewDto.getId(), "리뷰등록성공");
     }
 }
