da203b23191c9b9a7f7728fdfefa389d779945b7
hyeonju01 <tangled6648@gmail.com>
2023-01-30 17:09:50 +0900

    Feat: ReviewController 작성

---
+++ src/main/java/com/dev/museummate/controller/ReviewController.java
@@ -0,0 +1,42 @@
+package com.dev.museummate.controller;
+
+import com.dev.museummate.configuration.Response;
+import com.dev.museummate.domain.dto.review.WriteReviewRequest;
+import com.dev.museummate.domain.dto.review.WriteReviewResponse;
+import com.dev.museummate.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+@RequiredArgsConstructor
+@Slf4j
+public class ReviewController {
+
+  private final ReviewService reviewService;
+
+  /*
+  [X] 리뷰 등록
+   */
+  @PostMapping("/{exhibitionId}")
+  public Response<WriteReviewResponse> writeReview(@RequestBody WriteReviewRequest writeReviewRequest,
+      @PathVariable Long exhibitionId,
+      Authentication authentication) {
+    // authentication에서 name 추출
+    String email = authentication.getName();
+
+    // review service를 통해 review dto 생성
+    WriteReviewResponse writeReviewResponse =
+        reviewService.writeReview(email, writeReviewRequest, exhibitionId);
+
+    // writeReviewResponse 생성
+    return Response.success(writeReviewResponse);
+  }
+
+}
