c60dab6bbd670f3fbdc87ac7c063740c2476b807
hyeonju01 <tangled6648@gmail.com>
2023-01-30 16:04:51 +0900

    Feat: ReviewRepository 작성

--- src/main/java/com/dev/museummate/repository/ReviewRepository.java
+++ src/main/java/com/dev/museummate/repository/ReviewRepository.java
@@ -1,10 +1,7 @@
 package com.dev.museummate.repository;
 
+import com.dev.museummate.domain.entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/*
-전체 Entity 리팩토링 후 작성 예정
- */
-
-//public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-//}
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+}
