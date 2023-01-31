a11aecf3624dbea2d040726a7b0900cba95cf4b8
chlalswns200 <chlalswns200@naver.com>
2023-01-31 11:12:12 +0900

    feat: 함께 전시 관람 신청 기능 Repository 메서드 추가 findByUserIdAndGatheringId 추가

--- src/main/java/com/dev/museummate/repository/ParticipantRepository.java
+++ src/main/java/com/dev/museummate/repository/ParticipantRepository.java
@@ -1,8 +1,11 @@
 package com.dev.museummate.repository;
 
 import com.dev.museummate.domain.entity.ParticipantEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParticipantRepository extends JpaRepository<ParticipantEntity,Long> {
+public interface ParticipantRepository extends JpaRepository<ParticipantEntity, Long> {
+
+    Optional<ParticipantEntity> findByUserIdAndGatheringId(Long userId, Long gatheringId);
 
 }
