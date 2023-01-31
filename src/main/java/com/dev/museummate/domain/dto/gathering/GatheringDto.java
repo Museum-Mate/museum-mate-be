f8c6308d85f169eeeda4af4859b40cedb1962019
leejoonsang <djm04212@gmail.com>
2023-01-31 21:02:19 +0900

    feat: 필드 변수 추가

--- src/main/java/com/dev/museummate/domain/dto/gathering/GatheringDto.java
+++ src/main/java/com/dev/museummate/domain/dto/gathering/GatheringDto.java
@@ -3,6 +3,8 @@ package com.dev.museummate.domain.dto.gathering;
 import com.dev.museummate.domain.entity.ExhibitionEntity;
 import com.dev.museummate.domain.entity.GatheringEntity;
 import com.dev.museummate.domain.entity.UserEntity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +24,15 @@ public class GatheringDto {
     private String title;
     private String content;
     private Boolean close;
+
+    @ManyToOne
+    @JoinColumn(name = "exhibition_id")
+    private ExhibitionEntity exhibition;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
     private LocalDateTime createdAt;
     private LocalDateTime lastModifiedAt;
     private LocalDateTime deletedAt;
@@ -50,5 +61,25 @@ public class GatheringDto {
                               .user(user)
                               .exhibition(exhibition)
                               .build();
+    }
+
+    public static GatheringDto toDto(GatheringEntity gatheringEntity) {
+
+        return GatheringDto.builder()
+            .id(gatheringEntity.getId())
+            .meetDateTime(gatheringEntity.getMeetDateTime())
+            .meetLocation(gatheringEntity.getMeetLocation())
+            .maxPeople(gatheringEntity.getMaxPeople())
+            .title(gatheringEntity.getTitle())
+            .content(gatheringEntity.getContent())
+            .close(gatheringEntity.getClose())
+            .exhibition(gatheringEntity.getExhibition())
+            .user(gatheringEntity.getUser())
+            .createdAt(gatheringEntity.getCreatedAt())
+            .lastModifiedAt(gatheringEntity.getLastModifiedAt())
+            .deletedAt(gatheringEntity.getDeletedAt())
+            .createdBy(gatheringEntity.getCreatedBy())
+            .lastModifiedBy(gatheringEntity.getLastModifiedBy())
+            .build();
     }
 }
