d73c4d8120e2a2cb82db97102306692c2d88c2b7
hyeonju01 <tangled6648@gmail.com>
2023-02-01 17:14:23 +0900

    Feat: Service, Controller에 리뷰 수정 기능 추가

--- src/main/java/com/dev/museummate/service/ReviewService.java
+++ src/main/java/com/dev/museummate/service/ReviewService.java
@@ -1,5 +1,6 @@
 package com.dev.museummate.service;
 
+import com.dev.museummate.domain.dto.review.EditReviewRequest;
 import com.dev.museummate.domain.dto.review.ReviewDto;
 import com.dev.museummate.domain.dto.review.WriteReviewRequest;
 import com.dev.museummate.domain.entity.ExhibitionEntity;
@@ -10,10 +11,13 @@ import com.dev.museummate.exception.ErrorCode;
 import com.dev.museummate.repository.ExhibitionRepository;
 import com.dev.museummate.repository.ReviewRepository;
 import com.dev.museummate.repository.UserRepository;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ReviewService {
 
@@ -54,5 +58,39 @@ public class ReviewService {
         ReviewDto savedReviewDto = ReviewDto.toDto(review);
 
         return savedReviewDto;
+    }
+
+    /*
+    리뷰 수정 로직
+     */
+    public ReviewDto editReview(String authEmail,
+                                EditReviewRequest editReviewRequest,
+                                Long reviewId) {
+        // 유저 검증
+        UserEntity user = userRepository.findByEmail(authEmail)
+            .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, "Username Not Found."));
+
+        // 리뷰 검증
+        ReviewEntity reviewEntity = reviewRepository.findById(reviewId)
+            .orElseThrow(() -> new AppException(ErrorCode.CONTENT_NOT_FOUND, "Review Not Found."));
+
+        // 리뷰 작성자가 유저인지 검증
+        if (!Objects.equals(reviewEntity.getUser().getEmail(), authEmail)) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION, "");
+        }
+
+        // request를 entity로 변경
+        ReviewEntity editRequestToEntity = reviewEntity.toEntity(editReviewRequest);
+
+        // ReviewEntity 수정
+        reviewEntity.editReview(editRequestToEntity);
+
+        reviewEntity = reviewRepository.save(reviewEntity);
+
+        // 변경된 ReviewDto 생성
+        ReviewDto editedReviewDto = ReviewDto.toDto(reviewEntity);
+
+        // 변경된 ReviewDto 반환
+        return editedReviewDto;
     }
 }
