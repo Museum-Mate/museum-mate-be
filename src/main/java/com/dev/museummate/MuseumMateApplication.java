ed593106a0201fc9e1a6f877417831a14b6d6cb5
hyeonju01 <tangled6648@gmail.com>
2023-02-01 17:13:20 +0900

    Chore: @EnableJpaAuditing 어노테이션 위치 변경
    JpaAudit(@LastModifiedBy, @LastModifiedDate)일부 기능이 작동하지 않음
    해당 어노테이션을 MuseumMateApplication으로 옮겼을 때 작동함

--- src/main/java/com/dev/museummate/MuseumMateApplication.java
+++ src/main/java/com/dev/museummate/MuseumMateApplication.java
@@ -2,7 +2,10 @@ package com.dev.museummate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 @SpringBootApplication
 public class MuseumMateApplication {
 
