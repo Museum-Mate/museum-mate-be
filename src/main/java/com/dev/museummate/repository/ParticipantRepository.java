273f44735e31ba64932817b8874cf244303f04f2
leejoonsang <djm04212@gmail.com>
2023-02-02 02:56:34 +0900

    feat: 쿼리메소드 추가

--- src/main/java/com/dev/museummate/repository/ParticipantRepository.java
+++ src/main/java/com/dev/museummate/repository/ParticipantRepository.java
@@ -1,8 +1,13 @@
 package com.dev.museummate.repository;
 
+import com.dev.museummate.domain.entity.GatheringEntity;
 import com.dev.museummate.domain.entity.ParticipantEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipantRepository extends JpaRepository<ParticipantEntity,Long> {
+
+    Integer countByGatheringIdAndApproveTrue(Long gatheringId);
 
 }
