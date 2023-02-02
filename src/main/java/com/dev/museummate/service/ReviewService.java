2b0612378fd34375162d31478cf8762cc909fb3d
hyeonju01 <tangled6648@gmail.com>
2023-02-02 16:05:59 +0900

    Feat: 서비스 레이어에 리뷰 삭제 기능 구현

--- src/main/java/com/dev/museummate/service/ReviewService.java
+++ src/main/java/com/dev/museummate/service/ReviewService.java
@@ -1,5 +1,6 @@
 package com.dev.museummate.service;
 
+import com.dev.museummate.domain.UserRole;
 import com.dev.museummate.domain.dto.review.ReviewDto;
 import com.dev.museummate.domain.dto.review.WriteReviewRequest;
 import com.dev.museummate.domain.entity.ExhibitionEntity;
@@ -10,7 +11,10 @@ import com.dev.museummate.exception.ErrorCode;
 import com.dev.museummate.repository.ExhibitionRepository;
 import com.dev.museummate.repository.ReviewRepository;
 import com.dev.museummate.repository.UserRepository;
+import java.util.Collection;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -54,5 +58,29 @@ public class ReviewService {
         ReviewDto savedReviewDto = ReviewDto.toDto(review);
 
         return savedReviewDto;
+    }
+
+    // [] 리뷰 삭제
+    public ReviewDto deleteReview(Long reviewId, String userEmail, Collection<? extends GrantedAuthority> userRole) {
+
+        UserEntity reviewer = userRepository.findByEmail(userEmail) // 유저 검증
+                                        .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, String.format("Username %s Not Found.", userEmail)));
+
+        ReviewEntity reviewEntity = reviewRepository.findById(reviewId) // 리뷰 검증
+                                                    .orElseThrow(()
+                                                                     -> new AppException(ErrorCode.CONTENT_NOT_FOUND, String.format("Content %s Not Found.", reviewId)));
+
+        if(!Objects.equals(reviewEntity.getUser().getId(), reviewer.getId()) &&
+            !userRole.stream().findFirst().get().getAuthority().equals(UserRole.ROLE_ADMIN.toString())) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION, ""); // 리뷰 작성자 일치 여부 and 삭제자 권한 ADMIN 여부 검증
+        }
+
+        reviewRepository.delete(reviewEntity); //검증 통과 시 삭제
+
+        ReviewDto deletedReview = ReviewDto.builder() // 삭제 완료한 리뷰 번호로 DTO 생성하여 컨트롤러 레이어로 반환
+            .id(reviewId)
+            .build();
+
+        return deletedReview;
     }
 }
