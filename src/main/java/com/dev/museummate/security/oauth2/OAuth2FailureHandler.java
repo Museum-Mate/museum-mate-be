f90a0b247a39748bc14ae01797da0e2c192d793e
Geuun <jgk9612@gmail.com>
2023-02-09 02:58:22 +0900

    refactor: OAuth2 로그인 로직 변경
    로그인이 성공적으로 되지않아서 해당 로직의 리펙토링을 진행하였습니다.

--- src/main/java/com/dev/museummate/security/oauth2/OAuth2FailureHandler.java
+++ src/main/java/com/dev/museummate/security/oauth2/OAuth2FailureHandler.java
@@ -1,17 +1,19 @@
-package com.dev.museummate.security.oauth.handler;
+package com.dev.museummate.security.oauth2;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
-public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+@Slf4j
+@RequiredArgsConstructor
+public class OAuth2FailureHandler implements AuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
@@ -19,7 +21,7 @@ public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
         log.info("소셜 로그인 실패!");
         log.info("요청 페이지: {}", request.getRequestURI());
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.getWriter().write("소셜 로그인에 실패하였습니다. 서버로그를 확인해주세요 :)");
+        response.getWriter().write("소셜 로그인에 실패하였습니다. 확인 후 다시 시도해주세요 :)");
         response.sendRedirect("/");
     }
 }
