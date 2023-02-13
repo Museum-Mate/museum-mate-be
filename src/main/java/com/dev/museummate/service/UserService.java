cf127e03ec8996702310288992e9ae24482a3923
Jiyoung <ahn.jiyoung15@gmail.com>
2023-02-13 15:38:38 +0900

    Bug: 유저 정보 수정 시 기존 이름도 중복으로 처리하는 문제 해결

--- src/main/java/com/dev/museummate/service/UserService.java
+++ src/main/java/com/dev/museummate/service/UserService.java
@@ -141,12 +141,15 @@ public class UserService {
     @Transactional
     public String modifyUser(UserModifyRequest userModifyRequest, String email) {
 
-        userRepository.findByUserName(userModifyRequest.getUserName())
-                .ifPresent(user ->{
-                    throw new AppException(ErrorCode.DUPLICATE_USERNAME,String.format("%s는 중복 된 닉네임입니다.",userModifyRequest.getUserName()));
-                });
-
         UserEntity findUser = findUserByEmail(email);
+
+        if(!findUser.getUserName().equals(userModifyRequest.getUserName())) {
+            userRepository.findByUserName(userModifyRequest.getUserName())
+                          .ifPresent(user -> {
+                              throw new AppException(ErrorCode.DUPLICATE_USERNAME,
+                                                     String.format("%s는 중복 된 닉네임입니다.", userModifyRequest.getUserName()));
+                          });
+        }
 
         findUser.updateInfo(userModifyRequest);
 
